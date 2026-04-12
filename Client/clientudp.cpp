1e32fd85fd377a460e4d4ed67884ae8fb91214c9
agnesasuhodolli <agi.suhodolli@gmail.com>
2026-04-12 20:55:59 +0200

    Completion of parts

--- Client/clientudp.cpp
+++ Client/clientudp.cpp
@@ -5,3 +5,39 @@
 
 
 //Agnesa
+    while (true) {
+
+        std::cout << "\nCommand: ";
+        std::cin.getline(buffer, BUFFER_SIZE);
+
+        std::string input(buffer);
+
+        if (input == "exit") {
+            std::cout << "Disconnecting...\n";
+            break;
+        }
+
+        sendto(clientSocket, buffer, strlen(buffer), 0,
+               (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+        int n = recvfrom(clientSocket, buffer, BUFFER_SIZE, 0, nullptr, nullptr);
+
+        if (n < 0) {
+            std::cout << "No response from server\n";
+            continue;
+        }
+
+        buffer[n] = '\0';
+
+        std::cout << "Server: " << buffer << std::endl;
+    }
+
+#ifdef _WIN32
+    closesocket(clientSocket);
+    WSACleanup();
+#else
+    close(clientSocket);
+#endif
+
+    return 0;
+}
--- Server/serverudp.cpp
+++ Server/serverudp.cpp
@@ -2,6 +2,51 @@
 
 
 //Agnesa
+    while (true) {
 
+        int n = recvfrom(serverSocket, buffer, BUFFER_SIZE, 0,
+                         (sockaddr*)&clientAddr, &addrLen);
+
+        if (n <= 0) continue;
+
+        buffer[n] = '\0';
+        std::string msg(buffer);
+
+        std::string clientID = std::string(inet_ntoa(clientAddr.sin_addr))
+                             + ":" + std::to_string(ntohs(clientAddr.sin_port));
+
+        // Regjistrimi i klientëve
+        bool exists = false;
+        for (auto &c : clients) {
+            if (c.id == clientID) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            Client newClient;
+            newClient.id = clientID;
+            newClient.isAdmin = clients.empty();
+
+            clients.push_back(newClient);
+
+            std::cout << "New client: " << clientID;
+            if (newClient.isAdmin) std::cout << " (ADMIN)";
+            std::cout << std::endl;
+        }
+
+        // Gjej klientin aktual
+        Client currentClient;
+        for (auto &c : clients) {
+            if (c.id == clientID) {
+                currentClient = c;
+                break;
+            }
+        }
+
+        std::cout << clientID << " -> " << msg << std::endl;
+
+        std::string response;
 
 //Naila
